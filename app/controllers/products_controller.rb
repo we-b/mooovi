0d9900c8e487d62dee5c45f649216a2adebcf987
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2014-11-06 05:02:59 +0900

    fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ReviewController
   def show
     # productsテーブルから該当するidの作品情報を取得し@productの変数へ代入する処理を書いて下さい
+    @product = Product.new # 問題3ではこのコードは消して新しくコードを書いてください
   end
 end
