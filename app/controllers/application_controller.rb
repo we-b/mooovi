0b39189e812d7fbe84e46e954441e7223dbf32a1
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2014-11-07 17:00:26 +0900

    deviseで追加のパラメータを送れるようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up).push(:nickname, :avatar_file_name, :avatar_content_type, :avatar_file_size, :avatar_updated_at, :avatar)
+  end
 end
