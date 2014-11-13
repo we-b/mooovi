b812fd780404117e769c959c9442f7cce4e56e13
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2014-11-14 01:25:02 +0900

    ランキング機能

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -2,16 +2,19 @@ class ReviewController < ApplicationController
   layout 'review_site'
 
     
-  before_action :auth, :set_ranking
+  before_action :auth, :configure_ranking
 
   private
-  def set_ranking
-    @ranking = ReviewRank.new(5).get
-  end
 
   def auth
     authenticate_or_request_with_http_basic do |user,pass|
       user == 'tcamp' && pass == 'master'
     end
+  end
+
+  def configure_ranking
+  	ids = Review.limit(5).group(:product_id).order('count_product_id DESC').count(:product_id).keys
+  	sanitized_query = ActiveRecord::Base.send(:sanitize_sql_array, ["field(id ,?)", ids])
+  	@ranking = Product.where(:id => ids).order(sanitized_query)
   end
 end
