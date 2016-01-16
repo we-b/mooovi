d4b34a97e2809dc3a8e12ead2d6af4513f58525d
Takashi Nakagawa <tak1240@gmail.com>
2016-01-16 21:35:18 +0900

    update scraping model

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -1,11 +1,39 @@
 class Scraping
-  def self.get_products
-    puts 'get movies link URL'
-    # ここに処理を書く
+  def self.movie_urls
+    agent = Mechanize.new
+    links = []
+    next_url= "/now/"
+
+    while true
+      current_page = agent.get("http://eiga.com" + next_url)
+      elements = current_page.search('.m_unit h3 a')
+      elements.each do |ele|
+        links << ele.get_attribute('href')
+      end
+
+      next_link = current_page.at('.next_page')
+      next_url = next_link.get_attribute('href')
+
+      break unless next_url
+    end
+    links.each do |link|
+      get_product('http://eiga.com' + link)
+    end
   end
 
   def self.get_product(link)
-    puts 'get movie information'
-    # ここに処理を書く
+    agent = Mechanize.new
+    page = agent.get(link)
+    title = page.at('.moveInfoBox h1').inner_text
+    image_url = page.at('.pictBox img')[:src] if page.at('.pictBox img')
+    director = page.at('.f span').inner_text if page.at('.f span')
+    detail = page.at('.outline p').inner_text
+    open_date = page.at('.opn_date strong').inner_text if page.at('.opn_date strong')
+
+    product = Product.where(title: title, image_url: image_url).first_or_initialize
+    product.director = director
+    product.detail = detail
+    product.open_date = open_date
+    product.save
   end
 end
