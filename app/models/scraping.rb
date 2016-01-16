21e22972bbf5ba0eeae50ba7645ff78e706d8783
Takashi Nakagawa <tak1240@gmail.com>
2016-01-16 21:37:12 +0900

    simplify for deployment

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -26,14 +26,8 @@ class Scraping
     page = agent.get(link)
     title = page.at('.moveInfoBox h1').inner_text
     image_url = page.at('.pictBox img')[:src] if page.at('.pictBox img')
-    director = page.at('.f span').inner_text if page.at('.f span')
-    detail = page.at('.outline p').inner_text
-    open_date = page.at('.opn_date strong').inner_text if page.at('.opn_date strong')
 
     product = Product.where(title: title, image_url: image_url).first_or_initialize
-    product.director = director
-    product.detail = detail
-    product.open_date = open_date
     product.save
   end
 end
